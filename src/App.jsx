1c4295d3266d385b39d5b40c68a514f1519fbffb
Subham Kumar <53145627+subham0kumar@users.noreply.github.com>
2024-12-06 19:24:59 +0530

    story section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/Sections/About";
 import Features from "./Components/Sections/Features";
 import Hero from "./Components/Sections/Hero";
+import Story from "./Components/Sections/Story";
 import Navbar from "./Components/Utils/Navbar";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   );
 }
