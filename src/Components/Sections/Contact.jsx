89ec9b16f57fa691582ea9a2ab62b15d824c0a24
subham0kumar <subhamkumar9650@gmail.com>
2025-02-09 21:38:58 +0530

    Typo fixed

--- src/Components/Sections/Contact.jsx
+++ src/Components/Sections/Contact.jsx
@@ -12,8 +12,8 @@ const ImageClipBox = ({ src, clipClass }) => {
 const Contact = () => {
   return (
     <section id="contact" className="my-20 min-h-96 w-screen px-10">
-      <div className="relative rounded-lg bg-black py-24 taxt-blue-50 sm:overflow-hidden">
-        <div className="absolute -left-20 top-0 hiddden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
             src="img/contact-1.webp"
             clipClass="contact-clip-path-1"
@@ -24,7 +24,7 @@ const Contact = () => {
           />
         </div>
 
-        <div className="absolute -top-40 left-20 hiddden w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+        <div className="absolute -top-40 left-20 hidden w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
           <ImageClipBox
             src="img\swordman-partial.webp"
             clipClass="absolute md:scale-125"
@@ -40,7 +40,7 @@ const Contact = () => {
             Lets b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t
             <b>o</b>gether.
           </p>
-          <Button title='contact us' containerClass='mt-10 cursor-pointer' />
+          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
         </div>
       </div>
     </section>
